7c42b5827d8923ab820c7830210bf4a322b312c2
Sanchit <dhimansanchit5@gmail.com>
2025-02-13 22:50:26 +0530

    added functionality to the Digital-Clock Project with theme-Switch button

--- Digital-Clock/app.js
+++ Digital-Clock/app.js
@@ -1,15 +1,11 @@
-// <script>
-//         function updateClock() {
-//             const now = new Date();
-//             const hours = String(now.getHours()).padStart(2, '0');
-//             const minutes = String(now.getMinutes()).padStart(2, '0');
-//             const seconds = String(now.getSeconds()).padStart(2, '0');
-//             document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-//         }
-//         setInterval(updateClock, 1000);
-//         updateClock();
+const clock = document.getElementById("clock");
 
-//         document.getElementById('themeToggle').addEventListener('click', () => {
-//             document.body.classList.toggle('dark-mode');
-//         });
-//     </script>
+setInterval(function () {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
+
+const btn = document.querySelector("#themeToggle");
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
