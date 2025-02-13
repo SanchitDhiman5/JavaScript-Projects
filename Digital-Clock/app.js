befc87c38d5b998dd93edfd9c9171095aee11a9d
Sanchit <dhimansanchit5@gmail.com>
2025-02-13 21:58:06 +0530

    created UI of Digital-Clock

---
+++ Digital-Clock/app.js
@@ -0,0 +1,15 @@
+// <script>
+//         function updateClock() {
+//             const now = new Date();
+//             const hours = String(now.getHours()).padStart(2, '0');
+//             const minutes = String(now.getMinutes()).padStart(2, '0');
+//             const seconds = String(now.getSeconds()).padStart(2, '0');
+//             document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+//         }
+//         setInterval(updateClock, 1000);
+//         updateClock();
+
+//         document.getElementById('themeToggle').addEventListener('click', () => {
+//             document.body.classList.toggle('dark-mode');
+//         });
+//     </script>
