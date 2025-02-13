0edb763128130901c849b269207fb24409f21eb8
Sanchit <dhimansanchit5@gmail.com>
2025-02-13 17:41:37 +0530

    added inNaN() condition to check whether input is NaN or not, And all the querySelector elements will be accessed only after event will be trigger

--- BMI-Calculater/app.js
+++ BMI-Calculater/app.js
@@ -1,25 +1,34 @@
-const inputHeight = document.querySelector("#height");
-const inputWeight = document.querySelector("#weight");
 const calculateBmi = document.querySelector(".calculate-btn");
-const resultValue = document.querySelector(".result-value");
-const resultMsg = document.querySelector(".result-message");
-const resultContainer = document.querySelector(".result");
 
 calculateBmi.addEventListener("click", () => {
+  const inputHeight = document.querySelector("#height");
+  const inputWeight = document.querySelector("#weight");
+  const resultValue = document.querySelector(".result-value");
+  const resultMsg = document.querySelector(".result-message");
+  const resultContainer = document.querySelector(".result");
+
   const height = parseFloat(inputHeight.value);
   const weight = parseFloat(inputWeight.value);
+  resultContainer.style.display = "block";
 
   // Prevent Empty-Input Or -ve Values:
-  if (!height || !weight || height <= 0 || weight <= 0) {
-    resultContainer.style.display = "none";
+  if (
+    !height ||
+    !weight ||
+    height < 0 ||
+    weight < 0 ||
+    isNaN(height) ||
+    isNaN(weight)
+  ) {
+    resultValue.innerHTML = `Enter Valid Value ):`;
+    resultMsg.textContent = ``;
     return;
   }
 
   // Calculate BMI
-  const bmi = Math.round(weight / (height / 100) ** 2);
+  const bmi = (weight / (height / 100) ** 2).toFixed(2);
 
   // Show Result
-  resultContainer.style.display = "block";
   resultValue.textContent = bmi;
   resultMsg.textContent =
     bmi < 18.6 ? "Under-Weight" : bmi <= 24.9 ? "Normal" : "Over-Weight";
